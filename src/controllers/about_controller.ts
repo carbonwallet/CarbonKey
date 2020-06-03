cbc25e71dd39ae873068b160c3629585e62290f9
Ian Purton <ian.purton@gmail.com>
2020-06-03 08:29:56 +0200

    Tidy up styling.

--- src/controllers/about_controller.ts
+++ src/controllers/about_controller.ts
@@ -1,6 +1,11 @@
 import { Controller } from "stimulus"
 import { getHDWallet } from './util'
 
+interface SizedEvent {
+  width: number;
+  height: number;
+}
+
 export default class extends Controller {
 
   static targets = ['bitidAddress', 'xpubAddress', 'isIOS', 'isAndroidPersistant', 'isAdded']
@@ -16,4 +21,40 @@ export default class extends Controller {
     const xpub58 = getHDWallet().neutered().toBase58()
     this.xpubAddressTarget.innerText = xpub58.substr(xpub58.length - 8)
   }
+
+  isSizedEvent(e: any): e is SizedEvent {
+    return (e && e.width !== undefined && e.height !== undefined);
+  } 
+
+  scanFromFile(e : Event) {
+
+    const controller = this
+    /* global Image */
+    var img = new Image();
+    img.onload = function(event) {
+      if(controller.isSizedEvent(event)) {
+        console.log(event.width + " " + event.height);
+
+        // Convert to image data by drawing it into a canvas.
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext('2d');
+        canvas.width = event.width;
+        canvas.height = event.height;
+        context.drawImage(canvas, 0, 0);
+        var myData = context.getImageData(0, 0, img.width, img.height);
+
+        // scan for QRCode
+        const message = {
+            cmd: 'process',
+            width: event.width,
+            height: event.height,
+            imageData: myData
+        }
+      }
+    }
+  }
 }
+
+//qrcodeWorker.postMessage(message);
+//var _URL = window.URL || window.webkitURL;
+//img.src = _URL.createObjectURL(fileList[0]);
--- src/controllers/util.ts
+++ src/controllers/util.ts
@@ -1,14 +1,13 @@
 import * as bip39 from "bip39"
 import * as bip32 from "bip32"
-import * as bitcoin from 'bitcoinjs-lib'
 
 export function getHDWallet() : bip32.BIP32Interface {
 
     const seeds = window.localStorage.getItem('seed_words')
+    console.log('>> ' + seeds)
     if (seeds != null) {
       const seedBuffer = bip39.mnemonicToSeedSync(seeds)
       const hd = bip32.fromSeed(seedBuffer)
-  
       return hd.derivePath("m/0");
     }
 }
