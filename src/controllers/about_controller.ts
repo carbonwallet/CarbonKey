08133016b8b2c63871c6cd699de90d788f2cca4b
Ian Purton <ian.purton@gmail.com>
2020-06-05 11:10:00 +0000

    Scans from file uplaod.

--- src/controllers/about_controller.ts
+++ src/controllers/about_controller.ts
@@ -16,10 +16,28 @@ export default class extends Controller {
   readonly isAndroidPersistantTarget!: HTMLParagraphElement
   readonly isAddedTarget!: HTMLParagraphElement
 
+
+  qrcodeWorker = new Worker("qrcode-web-worker.js");
+
   connect() {
     console.log("About connected")
     const xpub58 = getHDWallet().neutered().toBase58()
     this.xpubAddressTarget.innerText = xpub58.substr(xpub58.length - 8)
+
+    // A web worker for running the main QR code parsing on
+    // a background thread.
+    this.qrcodeWorker.addEventListener('message', function (e) {
+
+      var resultData = e.data;
+
+      console.log(resultData)
+
+      if (resultData.result !== false) {
+
+        if (navigator.vibrate)
+          navigator.vibrate(200);
+      }
+    });
   }
 
   isSizedEvent(e: any): e is SizedEvent {
@@ -28,36 +46,31 @@ export default class extends Controller {
 
   scanFromFile(e : Event) {
 
-    console.log('Scan from file')
     const controller = this
     /* global Image */
     var img = new Image();
-    img.onload = function(event) {
-      if(controller.isSizedEvent(event)) {
-        console.log(event.width + " " + event.height);
+    img.onload = function() {
+        console.log(img.width + " " + img.height);
 
         // Convert to image data by drawing it into a canvas.
         var canvas = document.createElement('canvas');
         var context = canvas.getContext('2d');
-        canvas.width = event.width;
-        canvas.height = event.height;
-        context.drawImage(canvas, 0, 0);
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
         var myData = context.getImageData(0, 0, img.width, img.height);
 
         // scan for QRCode
         const message = {
             cmd: 'process',
-            width: event.width,
-            height: event.height,
+            width: img.width,
+            height: img.height,
             imageData: myData
         }
-      }
+
+        controller.qrcodeWorker.postMessage(message)
     }
     var _URL = window.URL || window.webkitURL;
     img.src = _URL.createObjectURL((<HTMLInputElement>e.target).files[0]);
   }
 }
-
-//qrcodeWorker.postMessage(message);
-//var _URL = window.URL || window.webkitURL;
-//img.src = _URL.createObjectURL(fileList[0]);
